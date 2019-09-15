ae53a45c831f4dab83d25f9f819dc2a8ae4802bf
soul-catcher <dmitriy.samsonov28@gmail.com>
2019-09-15 23:27:51 +0700

    Little changes

--- avs/lab1/pcinfo.sh
+++ avs/lab1/pcinfo.sh
@@ -14,35 +14,38 @@ echo -ne "\t* "
 lscpu | grep "Имя модели:" | cut -d'@' -f1
 echo -ne "\t* "
 lscpu | grep "Архитектура:"
-echo -ne "\t* Тактовая частота:   "
+echo -ne "\t* Тактовая частота:\t\t  "
 lscpu | grep "Имя модели:" | cut -d'@' -f2
-echo -ne "\t* Количество ядер:     "
+echo -ne "\t* Количество ядер:\t\t   "
 lscpu | grep -m1 "CPU(s):" | rev | cut -d' ' -f1 | rev
-echo -ne "\t* Количество потоков\n\t  на одно ядро:\t       "
+echo -ne "\t* Количество потоков\n\t  на одно ядро:\t\t\t   "
 lscpu | grep -m1 "Thread(s) per core:" | rev | cut -d' ' -f1 | rev
 
 echo "Оперативная память:"
-echo -ne "\t* Всего:\t       "
+echo -ne "\t* Всего:\t\t\t   "
 free -h | grep "Mem:" | tr -s ' ' | cut -d' ' -f2
-echo -ne "\t* Доступно:\t       "
+echo -ne "\t* Доступно:\t\t\t   "
 free -h | grep "Mem:" | tr -s ' ' | cut -d' ' -f7
 
 echo "Жёсткий диск:"
-echo -ne "\t* Всего:\t       "
-df -h | grep "/dev/sda1" | tr -s ' ' | cut -d' ' -f2
-echo -ne "\t* Доступно:\t       "
-df -h | grep "/dev/sda1" | tr -s ' ' | cut -d' ' -f4
-echo -ne "\t* Смонтировано в\n\t  корневую директорию: "
-df -h | grep "/dev/sda1" | tr -s ' ' | cut -d' ' -f1
-echo -ne "\t* SWAP всего:\t       "
+echo -ne "\t* Всего:\t\t\t   "
+df -h | grep "/dev/nvme0n1p2" | tr -s ' ' | cut -d' ' -f2
+echo -ne "\t* Доступно:\t\t\t   "
+df -h | grep "/dev/nvme0n1p2" | tr -s ' ' | cut -d' ' -f4
+echo -ne "\t* Смонтировано в\n\t  корневую директорию:\t\t   "
+df -h | grep "/dev/nvme0n1p2" | tr -s ' ' | cut -d' ' -f1
+echo -ne "\t* SWAP всего:\t\t\t   "
 free -h | grep "Swap:" | tr -s ' ' | cut -d' ' -f2
-echo -ne "\t* SWAP доступно:       "
+echo -ne "\t* SWAP доступно:\t\t   "
 free -h | grep "Swap:" | tr -s ' ' | cut -d' ' -f4
 
 echo "Сетевые интерфейсы:"
 echo -ne "\t* Количество сетевых\n\t  интерфейсов:\t       "
 ls /sys/class/net/ | wc -l
-for var in /sys/class/net/*
-do
+echo -e "\nИмя сетевого интерфейса\t\tMAC адрес\t\tIP адрес    Скорость соединения"
+for var in /sys/class/net/*; do
+  echo -ne "$var\t\t"
+  cat "$var/address" | tr "\n" "\t"
 
+  cat $var/speed | tr "\n" "\t"
 done
