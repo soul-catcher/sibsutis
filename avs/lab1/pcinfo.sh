5123b538cea51560eeee6297074b0498bcdd90d3
soul-catcher <dmitriy.samsonov28@gmail.com>
2019-09-17 13:57:40 +0700

    Done avs/lab1

--- avs/lab1/pcinfo.sh
+++ avs/lab1/pcinfo.sh
@@ -33,19 +33,19 @@ df -h | grep "/dev/nvme0n1p2" | tr -s ' ' | cut -d' ' -f2
 echo -ne "\t* Доступно:\t\t\t   "
 df -h | grep "/dev/nvme0n1p2" | tr -s ' ' | cut -d' ' -f4
 echo -ne "\t* Смонтировано в\n\t  корневую директорию:\t\t   "
-df -h | grep "/dev/nvme0n1p2" | tr -s ' ' | cut -d' ' -f1
+df -h | grep "/$" | tr -s ' ' | cut -d' ' -f1
 echo -ne "\t* SWAP всего:\t\t\t   "
 free -h | grep "Swap:" | tr -s ' ' | cut -d' ' -f2
 echo -ne "\t* SWAP доступно:\t\t   "
 free -h | grep "Swap:" | tr -s ' ' | cut -d' ' -f4
 
 echo "Сетевые интерфейсы:"
-echo -ne "\t* Количество сетевых\n\t  интерфейсов:\t       "
+echo -ne "\t* Количество сетевых\n\t  интерфейсов:\t\t\t   "
 ls /sys/class/net/ | wc -l
-echo -e "\nИмя сетевого интерфейса\t\tMAC адрес\t\tIP адрес    Скорость соединения"
+echo -e "\nИмя сетевого интерфейса\t\tMAC адрес\t\tIP адрес\t\tСкорость соединения"
 for var in /sys/class/net/*; do
-  echo -ne "$var\t\t"
-  cat "$var/address" | tr "\n" "\t"
-
-  cat $var/speed | tr "\n" "\t"
+  echo -ne "${var##*/}\t\t\t\t"
+  tr "\n" "\t" < "$var/address"
+  printf "%-24s" "$(ip -4 -o a | grep " ${var##*/} " | tr -s ' ' | cut -d' ' -f4 | tr -d '\n')"
+  ip -4 -0 a | grep " ${var##*/}: " | tr -s ' ' | cut -d' ' -f13
 done
