3323d3d48ba56699be19bdef1d2da251f9a6b742
soul-catcher <dmitriy.samsonov28@gmail.com>
2019-11-06 17:32:45 +0700

    Done 2/3 of saod/cource/29

--- saod/course/29.cpp
+++ saod/course/29.cpp
@@ -177,11 +177,188 @@ void show_list(Record *ind_arr[], int n = N) {
     }
 }
 
+// Функция для сравнения строк, возвращает -1, если str1 < str2, 1, если str1 > str2 и 0, если они равны
+int strcomp(const string &str1, const string &str2, int len = -1) {
+    if (len == -1) {
+        len = (int) str1.length();
+    }
+    for (int i = 0; i < len; ++i) {
+        if (str1[i] == '\0' and str2[i] == '\0') {
+            return 0;
+        } else if (str1[i] == ' ' and str2[i] != ' ') {
+            return -1;
+        } else if (str1[i] != ' ' and str2[i] == ' ') {
+            return 1;
+        } else if (str1[i] < str2[i]) {
+            return -1;
+        } else if (str1[i] > str2[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+// Двоичный поиск
+int quick_search(Record *arr[], const string &key) {
+    int l = 0;
+    int r = N - 1;
+    while (l < r) {
+        int m = (l + r) / 2;
+        if (strcomp(arr[m]->street, key, 3) < 0) {
+            l = m + 1;
+        } else {
+            r = m;
+        }
+    }
+    if (strcomp(arr[r]->street, key, 3) == 0) {
+        return r;
+    }
+    return -1;
+}
+
+// Реализация поиска с построением очереди
+void search(Record *arr[], int &ind, int &n) {
+    Node *head = nullptr, *tail = nullptr;
+    string key;
+    do {
+        key = prompt("Input search key (3 characters of street)");
+    } while (key.length() != 3);
+    ind = quick_search(arr, key);
+    if (ind == -1) {
+        cout << "Not found\n";
+    } else {
+        head = new Node{*arr[ind], nullptr};
+        tail = head;
+        int i;
+        for (i = ind + 1; i < 4000 and strcomp(arr[i]->street, key, 3) == 0; ++i) {
+            tail->next = new Node{*arr[i], nullptr};
+            tail = tail->next;
+        }
+        n = i - ind;
+        auto find_arr = new Record *[n];
+        make_index_array(find_arr, head, n);
+        show_list(find_arr, n);
+        delete[] find_arr;
+    }
+}
+
+// Вершина дерева
+struct Vertex {
+    Record *data;
+    Vertex *left;
+    Vertex *right;
+    int balance;
+};
+
+// Добавление записи в дерево
+void dbd_add(Record *data, Vertex *&p) {
+    static int vr = 1;
+    static int hr = 1;
+    if (!p) {
+        p = new Vertex{data, NULL, NULL, 0};
+        vr = 1;
+    } else if (strcomp(data->fio, p->data->fio) < 0) {
+        dbd_add(data, p->left);
+        if (vr == 1) {
+            if (p->balance == 0) {
+                Vertex *q = p->left;
+                p->left = q->right;
+                q->right = p;
+                p = q;
+                q->balance = 1;
+                vr = 0;
+                hr = 1;
+            } else {
+                p->balance = 0;
+                vr = 1;
+                hr = 0;
+            }
+        } else {
+            hr = 0;
+        }
+    } else if (strcomp(data->fio, p->data->fio) >= 0) {
+        dbd_add(data, p->right);
+        if (vr == 1) {
+            p->balance = 1;
+            hr = 1;
+            vr = 0;
+        } else if (hr == 1) {
+            if (p->balance == 1) {
+                Vertex *q = p->right;
+                p->balance = 0;
+                q->balance = 0;
+                p->right = q->left;
+                q->left = p;
+                p = q;
+                vr = 1;
+                hr = 0;
+            } else {
+                hr = 0;
+            }
+        }
+    }
+}
+
+// Вывод дерева на экран
+void Print_tree(Vertex *p, int &i) {
+    if (p) {
+        Print_tree(p->left, i);
+        print_record(p->data, i++);
+        Print_tree(p->right, i);
+    }
+}
+
+// Поиск в дереве
+void search_in_tree(Vertex *root, const string &key, int &i) {
+    if (root) {
+        if (strcomp(key, root->data->fio) < 0) {
+            search_in_tree(root->left, key, i);
+        } else if (strcomp(key, root->data->fio) > 0) {
+            search_in_tree(root->right, key, i);
+        } else if (strcomp(key, root->data->fio) == 0) {
+            search_in_tree(root->left, key, i);
+            print_record(root->data, i++);
+            search_in_tree(root->right, key, i);
+        }
+    }
+}
+
+// Очистка памяти и удаление дерева
+void rmtree(Vertex *root) {
+    if (root) {
+        rmtree(root->right);
+        rmtree(root->left);
+        delete root;
+    }
+}
+
+// Построение дерева по ФИО
+void tree(Record *arr[], int n) {
+    Vertex *root = NULL;
+    for (int i = 0; i < n; ++i) {
+        dbd_add(arr[i], root);
+    }
+    print_head();
+    int i = 1;
+    Print_tree(root, i);
+    string key;
+    do {
+        key = prompt("Input fio, 0 - exit");
+        print_head();
+        i = 1;
+        search_in_tree(root, key, i);
+    } while (key != "0");
+    rmtree(root);
+}
+
 // Основной цикл (главное меню программы)
 void mainloop(Record *unsorted_ind_array[], Record *sorted_ind_array[]) {
+    int search_ind, search_n = -1;
     while (true) {
         string chose = prompt("1: Show unsorted list\n"
                               "2: Show sorted list\n"
+                              "3: Search\n"
+                              "4: Tree\n"
                               "Any key: Exit");
         switch (chose[0]) {
             case '1':
@@ -189,6 +366,16 @@ void mainloop(Record *unsorted_ind_array[], Record *sorted_ind_array[]) {
                 break;
             case '2':
                 show_list(sorted_ind_array);
+                break;
+            case '3':
+                search(sorted_ind_array, search_ind, search_n);
+                break;
+            case '4':
+                if (search_n == -1) {
+                    cout << "Please search anything before making tree\n";
+                } else {
+                    tree(&sorted_ind_array[search_ind], search_n);
+                }
                 break;
             default:
                 return;
