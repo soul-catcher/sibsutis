860a7ac82144a326f7a245e022e43376e31aae98
Dmitry Samsonov <dmitriy.samsonov28@gmail.com>
2020-03-17 02:48:08 +0700

    Сделана четвёртая лаба по андроиду

---
+++ android/lab4/app/src/main/java/ru/sibsutis/soul_catcher/lab3/MainActivity.java
@@ -0,0 +1,89 @@
+package ru.sibsutis.soul_catcher.lab3;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+    //Объявляем картинку для компаса
+    private ImageView HeaderImage;
+    //Объявляем функцию поворота картинки
+    private float RotateDegree = 0f;
+    //Объявляем работу с сенсором устройства
+    private SensorManager mSensorManager;
+    //Объявляем объект TextView
+    TextView CompOrient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Связываем объект ImageView с нашим изображением:
+        HeaderImage = (ImageView) findViewById(R.id.CompassView);
+
+        //TextView в котором будет отображаться градус поворота:
+        CompOrient = (TextView) findViewById(R.id.Header);
+
+        //Инициализируем возможность работать с сенсором устройства:
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //Устанавливаем слушателя ориентации сенсора
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //Останавливаем при надобности слушателя ориентации
+        //сенсора с целью сбережения заряда батареи:
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        //Получаем градус поворота от оси, которая направлена на север, север = 0 градусов:
+        float degree = Math.round(event.values[0]);
+        CompOrient.setText("Отклонение от севера: " + Float.toString(degree) + " градусов");
+
+        //Создаем анимацию вращения:
+        RotateAnimation rotateAnimation = new RotateAnimation(
+                RotateDegree,
+                -degree,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f);
+
+        //Продолжительность анимации в миллисекундах:
+        rotateAnimation.setDuration(200);
+
+        //Настраиваем анимацию после завершения подсчетных действий датчика:
+        rotateAnimation.setFillAfter(true);
+
+        //Запускаем анимацию:
+        HeaderImage.startAnimation(rotateAnimation);
+        RotateDegree = -degree;
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //Этот метод не используется, но без него программа будет ругаться
+    }
+}
