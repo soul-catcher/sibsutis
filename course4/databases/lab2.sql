3cd975321c66bd4e8fa2b0566b4ee76ad0c9a19d
Dmitry Samsonov <dmitriy.samsonov28@gmail.com>
2020-09-28 14:14:28 +0700

    Небольшие исправления во второй лабе по БД

--- course4/databases/lab2.sql
+++ course4/databases/lab2.sql
@@ -1,17 +1,20 @@
 select * from cust
-where cname between 'A%' and 'G%';
+where left(cname, 1) between 'A' and 'G';
 
 select * from sal
 where sname like '%e%';
 
-select sum(amt) from ord;
+select sum(amt) from ord
+where odate = '03-OCT-90';
 
 select count(distinct city) from cust;
 
-select cname, o.*
-from cust c inner join ord o on c.cnum = o.cnum
-where amt = (
-    select min(amt)
-    from ord
-    where cnum = c.cnum
-);
+select cname, o.onum, min(o.amt), o.odate, o.cnum, o.snum
+from cust c inner join ord o using (cnum)
+group by cnum;
+
+select cnum, max(cname) cname, city, rating, snum from cust
+where left(cname, 1) = 'G';
+
+select city, max(rating) rating from cust
+group by city;
