5516b7bb05e095c84e86792130bddf7ee5ce6a24
Dmitry Samsonov <dmitriy.samsonov28@gmail.com>
2020-10-21 18:51:26 +0700

    Сделана 2 лаба по ТЯП

---
+++ course4/theory_of_pl/lab2/lab2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+automate = {}
+with open('automate.txt') as f:
+    start = f.readline().split()[1]
+    end = frozenset(f.readline().split()[1:])
+    header = f.readline().split()
+    non_term_inds = dict(map(reversed, enumerate(header)))
+    for line in f:
+        automate[line[0]] = tuple(line[1:].split())
+
+
+def launch_automate(seq) -> None:
+    if (state := start) not in automate:
+        print('Ошибка! Неверно задано начальное состояние! Оно отсутствует в таблице!')
+        return
+    for i, char in enumerate(seq):
+        print(f'({state},{seq[i:]}) ├─ ', end='')
+        if (transition := automate.get(state)) is None:
+            print(f'\nОшибка! Состояние "{state}" отсутствует в таблице! Переход невозможен!')
+            return
+        if (idx := non_term_inds.get(char)) is None:
+            print(f'\nОшибка! В цепочке встречен посторонний символ "{char}", отсутствующий в таблице!')
+            return
+        if (new_state := transition[idx]) == '-':
+            print(f'\nОшибка: Отсутствует переход из состояния "{state}" по символу "{char}"')
+            return
+        state = new_state
+
+    if state in end:
+        print(f'({state},λ)')
+        print('Автомат допускает цепочку', seq)
+    else:
+        print('\nЦепочка кончилась, но автомат не пришёл в заключительное состояние!')
+
+
+while True:
+    try:
+        seq = input('Введите цепочку. Для выхода из программы нажмите ^C\n> ')
+    except KeyboardInterrupt:
+        print()
+        break
+    launch_automate(seq)
