8c9848215b6abe7aefed4900def6b2a2b2563d52
Dmitry Samsonov <dmitriy.samsonov28@gmail.com>
2021-03-10 21:26:09 +0700

    [ТИ] Доделаны 4 и 5 лабы

--- 4course/information_theory/lab4-5.py
+++ 4course/information_theory/lab4-5.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-from pprint import pprint
-
 from lib import *
 
 f1 = open('f1.txt').readline().removesuffix('\n')
@@ -9,12 +7,23 @@ f2 = open('f2.txt').readline().removesuffix('\n')
 dog_hearth = prepare_text(open('dog-hearth.txt').read())
 code = open('lib.py').read()
 
-for text in f1, f2, dog_hearth, code:
+for i, text in enumerate((f1, f2, dog_hearth, code), 1):
+    print(f' text {i} '.center(80, '='))
+    frequencies = get_frequencies(text)
+    text_entropy = entropy(frequencies.values())
     for n in range(2, 5):
-        code_table = huffman(get_frequencies(text), n)
-        encoded_text = encode(text, code_table)
+        print(f' encoded alphabet len = {n} '.center(40, '*'))
+        code_table = huffman(frequencies, n)
+        if n == 2:
+            redundancy = sum(map(len, code_table.values())) / len(code_table) - text_entropy
+            print(f'{redundancy = }')
 
-        # for i in range(1, 4):
-        #     subsequences = [list(gen_subsequences(list(code.values()), i)) for code in codes]
-        #     entropys = list(map(entropy, (get_frequencies(list(subsequence)).values() for subsequence in subsequences)))
-        #     pprint(entropys)
+        encoded_text = ''.join(map(str, encode(text, code_table)))
+        for sub_len in range(1, 4):
+            subs = list(gen_subsequences(encoded_text, sub_len))
+            sub_freqs = get_frequencies(subs)
+            print(
+                f'{sub_len = }',
+                f'Entropy = {entropy(sub_freqs.values()) / sub_len}',
+                sep='\n'
+            )
